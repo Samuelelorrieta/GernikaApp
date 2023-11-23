067e0a500af7bbf199653a7a280c5bd51e86ed47
David070804 <david.lopezcr@elorrieta-errekamari.com>
2023-11-23 10:16:15 +0100

    Icono

---
+++ app/src/main/java/com/example/gernikaapp/Bunker.java
@@ -0,0 +1,16 @@
+package com.example.gernikaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bunker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bunker);
+
+
+    }
+}
